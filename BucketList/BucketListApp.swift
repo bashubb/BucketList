c3f697c86d4728e0ca985737c7154c5886802726
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2023-10-31 12:33:51 +0100

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by HubertMac on 31/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
