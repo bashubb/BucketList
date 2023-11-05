8d12a4b5766c4638488310919243d9b29ed78265
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2023-11-05 20:22:01 +0100

    playing with MapKit

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -6,54 +6,44 @@
 //
 
 import SwiftUI
+import MapKit
 
-struct ContentView: View {
-    
-    enum LoadingState{
-        case loading, success, failed
-    }
-    
-    struct LoadingView: View {
-        var body: some View {
-            Text("Loading...")
-        }
-    }
-    
-    struct SuccessView: View {
-        var body: some View {
-            Text("Success!")
-        }
-    }
-    
-    struct FailedView: View {
-        var body: some View {
-            Text("Failed.")
-        }
-    }
-    
-    var body: some View {
-        var loadingState = LoadingState.loading
-        
-    
-            switch loadingState {
-            case .loading:
-                LoadingView()
-            case .success:
-                SuccessView()
-            case .failed:
-                FailedView()
-            }
-            
-            
-//                .onTapGesture {
-//                    FileManager.default.encodeToFile(content: "message", fileName: "test.txt")
-//                    let message = FileManager.default.decode(String.self, from: "test.txt")
-//                    print(message)
-//                }
-      
-    }
+struct Location: Identifiable {
+    let id = UUID()
+    let name: String
+    let coordinate: CLLocationCoordinate2D
 }
 
+struct ContentView: View {
+    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+    
+    let locations = [
+        Location(name: "Buckingam Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
+        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
+    ]
+    var body: some View {
+        VStack{
+            NavigationStack {
+                
+                Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
+                    MapAnnotation(coordinate: location.coordinate) {
+                        NavigationLink {
+                            Text(location.name)
+                        } label: {
+                            Circle()
+                                .stroke(Color.red)
+                                .frame(width: 44,height: 44)
+                            
+                        }
+                        
+                    }
+                }
+            }
+        }
+    }
+    
+    
+}
 #Preview {
     ContentView()
 }
