012680e9122a8ea501409d3a2388b10943b69df8
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2023-11-05 06:44:15 +0100

    Extension for FileManager, play arround with enums

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -8,14 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    
+    enum LoadingState{
+        case loading, success, failed
+    }
+    
+    struct LoadingView: View {
+        var body: some View {
+            Text("Loading...")
         }
-        .padding()
+    }
+    
+    struct SuccessView: View {
+        var body: some View {
+            Text("Success!")
+        }
+    }
+    
+    struct FailedView: View {
+        var body: some View {
+            Text("Failed.")
+        }
+    }
+    
+    var body: some View {
+        var loadingState = LoadingState.loading
+        
+    
+            switch loadingState {
+            case .loading:
+                LoadingView()
+            case .success:
+                SuccessView()
+            case .failed:
+                FailedView()
+            }
+            
+            
+//                .onTapGesture {
+//                    FileManager.default.encodeToFile(content: "message", fileName: "test.txt")
+//                    let message = FileManager.default.decode(String.self, from: "test.txt")
+//                    print(message)
+//                }
+      
     }
 }
 
---
+++ BucketList/extensions.swift
@@ -0,0 +1,60 @@
+//
+//  extension FileManager.swift
+//  BucketList
+//
+//  Created by HubertMac on 31/10/2023.
+//
+
+import Foundation
+
+extension FileManager {
+    private func getDocumentsDirectory() -> URL {
+        // find full path of directory
+        let paths = self.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
+    func encodeToFile<T: Encodable>(content: T, fileName: String) {
+        let url = getDocumentsDirectory().appendingPathComponent(fileName)
+        let encoder = JSONEncoder()
+        
+        do {
+            let data = try encoder.encode(content)
+            let jsonString = String( decoding: data, as: UTF8.self)
+            try jsonString.write(to: url, atomically: true, encoding: .utf8)
+            
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    
+    func decode<T: Decodable>(_ type: T.Type, from fileName: String) -> T {
+        
+        let url = getDocumentsDirectory().appending(path: fileName)
+
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to load \(fileName) from bundle.")
+        }
+
+        let decoder = JSONDecoder()
+        
+
+        do {
+            return try decoder.decode(T.self, from: data)
+        } catch DecodingError.keyNotFound(let key, let context) {
+            fatalError("Failed to decode \(fileName) from bundle due to missing key '\(key.stringValue)' not found – \(context.debugDescription)")
+        } catch DecodingError.typeMismatch(_, let context) {
+            fatalError("Failed to decode \(fileName) from bundle due to type mismatch – \(context.debugDescription)")
+        } catch DecodingError.valueNotFound(let type, let context) {
+            fatalError("Failed to decode \(fileName) from bundle due to missing \(type) value – \(context.debugDescription)")
+        } catch DecodingError.dataCorrupted(_) {
+            fatalError("Failed to decode \(fileName) from bundle because it appears to be invalid JSON")
+        } catch {
+            fatalError("Failed to decode \(fileName) from bundle: \(error.localizedDescription)")
+        }
+    }
+        
+}
+
+
