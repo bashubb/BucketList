20db8039c39c904bbd22b0788bb6ec489fe68dd7
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2023-11-06 06:30:59 +0100

    playing with biometric authenticate

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -6,42 +6,41 @@
 //
 
 import SwiftUI
-import MapKit
-
-struct Location: Identifiable {
-    let id = UUID()
-    let name: String
-    let coordinate: CLLocationCoordinate2D
-}
+import LocalAuthentication
 
 struct ContentView: View {
-    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+    @State private var isUnlocked = false
     
-    let locations = [
-        Location(name: "Buckingam Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
-        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
-    ]
     var body: some View {
         VStack{
-            NavigationStack {
-                
-                Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
-                    MapAnnotation(coordinate: location.coordinate) {
-                        NavigationLink {
-                            Text(location.name)
-                        } label: {
-                            Circle()
-                                .stroke(Color.red)
-                                .frame(width: 44,height: 44)
-                            
-                        }
-                        
-                    }
-                }
+            if isUnlocked {
+                Text("Unlocked")
+            } else {
+              Text("Locked")
             }
         }
+        .onAppear(perform: authenticate)
     }
     
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "We need to unlock your data."
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                if success {
+                    isUnlocked = true
+                } else {
+                    // there problem
+                }
+                
+            }
+        } else {
+            //device has no biometrics
+        }
+    }
     
 }
 #Preview {
