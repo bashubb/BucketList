ec036585c56cbababa1b843dc7b8eb42aafd6024
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2023-11-07 19:43:57 +0100

    Custom Map Annotation, Relocate coordinate to the model,  preview example, custom equal function

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -16,7 +16,19 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             Map(coordinateRegion:$mapRegion, annotationItems: locations) { location in
-                MapMarker(coordinate: CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude))
+                MapAnnotation(coordinate: location.coordinate ){
+                    VStack {
+                        Image(systemName: "star.circle")
+                            .resizable()
+                            .foregroundStyle(Color.red)
+                            .frame(width: 44, height: 44)
+                            .background(.white)
+                            .clipShape(Circle())
+                        
+                        Text(location.name)
+            
+                    }
+                }
             }
                 .ignoresSafeArea()
             
